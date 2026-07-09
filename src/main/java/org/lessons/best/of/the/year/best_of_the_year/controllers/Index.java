833344cef28969b58873e7937c82228739880225
alessiaimprota <alessiaimprota126@gmail.com>
2026-07-09 18:04:21 +0200

    first commit

---
+++ src/main/java/org/lessons/best/of/the/year/best_of_the_year/controllers/Index.java
@@ -0,0 +1,15 @@
+package org.lessons.best.of.the.year.best_of_the_year.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Index {
+
+    @GetMapping
+    public String Homepage() {
+        return "home";
+    }
+}
